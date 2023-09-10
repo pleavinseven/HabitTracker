e31b566e08d7e9f7dde5593bd2c2dee07476f881
pleavin <pleavinseven@gmail.com>
2023-09-10 21:05:19 +0200

    compose version set

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,15 +52,17 @@ android {
 
 dependencies {
 
+    val composeVersion = "2.6.1"
+
     implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$composeVersion")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$composeVersion")
     implementation("androidx.activity:activity-ktx:1.7.2")
 
     val roomVersion = "2.5.2"
