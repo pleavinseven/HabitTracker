a70455c5afeee92499ed4e1763b25d637afe86bf
pleavin <pleavinseven@gmail.com>
2023-09-12 14:09:12 +0200

    test implementations added for coroutines.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,8 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     testImplementation("io.mockk:mockk:1.13.5")
