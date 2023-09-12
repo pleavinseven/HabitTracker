197551eda81e0d2d9051622a6f9ce57fa13d2dff
pleavin <pleavinseven@gmail.com>
2023-09-12 14:14:06 +0200

    coroutine scope added to tests.

--- app/src/test/java/com/pleavinseven/MainViewModelTest.kt
+++ app/src/test/java/com/pleavinseven/MainViewModelTest.kt
@@ -1,38 +1,53 @@
 package com.pleavinseven
 
 import android.app.Application
-import androidx.room.Room
-import androidx.test.platform.app.InstrumentationRegistry
+import com.pleavinseven.model.database.HabitDao
 import com.pleavinseven.model.database.Repository
-import com.pleavinseven.model.database.TestDatabase
+import com.pleavinseven.model.database.TimeLogDao
 import io.mockk.every
 import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 
 class MainViewModelTest {
 
-    private val applicationMock = mockk<Application>() {
+    private val applicationMock = mockk<Application> {
         every { applicationContext } returns mockk()
     }
+    private val timeLogDao = mockk<TimeLogDao>(relaxed = true)
+    private val habitDao = mockk<HabitDao>(relaxed = true)
+    private lateinit var repository: Repository
+    private lateinit var viewModel: MainViewModel
 
-    private val database = Room.inMemoryDatabaseBuilder(
-        InstrumentationRegistry.getInstrumentation().context, TestDatabase::class.java
-    ).build()
-    private val timeLogDao = database.timeLogDao()
-    private val repository = Repository(timeLogDao)
-    private val viewModel = MainViewModel(repository, applicationMock)
+    @Before
+    fun setUp() {
+        repository = Repository(timeLogDao, habitDao)
+        viewModel = MainViewModel(repository, applicationMock)
+    }
 
     @Test
     fun countStartsAtZero() {
         assertEquals(0, viewModel.count)
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun onCountButtonClicked_CountPlusOne() {
-        viewModel.onCountButtonClicked()
+    fun onCountButtonClicked_CountPlusOne() = runTest {
+        Dispatchers.setMain(Dispatchers.Unconfined)
+        launch { viewModel.onCountButtonClicked() }
+        advanceUntilIdle()
         assertEquals(1, viewModel.count)
-        viewModel.onCountButtonClicked()
-        assertEquals(2, viewModel.count)
+    }
+
+    @Test
+    fun beforeCallingOnClick_TimeLogsAreEmpty() {
+        assert(viewModel.timeLogsList.isEmpty())
     }
 }
