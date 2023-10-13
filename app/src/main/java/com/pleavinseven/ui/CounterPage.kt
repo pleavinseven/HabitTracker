d7a2cda55c1595a82561739ae14c48f37bb79cf9
pleavin <pleavinseven@gmail.com>
2023-10-13 18:45:08 +0200

    add habit dialog changed to include goal and icon buttons

--- app/src/main/java/com/pleavinseven/ui/CounterPage.kt
+++ app/src/main/java/com/pleavinseven/ui/CounterPage.kt
@@ -13,30 +13,50 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Remove
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.pleavinseven.R
+import com.pleavinseven.model.entities.Habit
+import com.pleavinseven.utils.Utils
 import com.pleavinseven.viewmodels.MainViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun CounterPage(viewModel: MainViewModel, habitId: Int) {
@@ -46,10 +66,16 @@ fun CounterPage(viewModel: MainViewModel, habitId: Int) {
     var count by remember {
         mutableIntStateOf(startCount)
     }
+    var showPopupWindow by remember {
+        mutableStateOf((false))
+    }
     viewModel.getTimeLogs(habitName)
     Box(
         modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
     ) {
+        if (showPopupWindow) {
+            EditHabitDialog(viewModel, habit) { showPopupWindow = false }
+        }
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -80,7 +106,7 @@ fun CounterPage(viewModel: MainViewModel, habitId: Int) {
                     Icon(
                         modifier = Modifier.size(160.dp),
                         imageVector = Icons.Filled.Remove,
-                        contentDescription = stringResource(id = R.string.add_habit_description)
+                        contentDescription = stringResource(id = R.string.count_minus_one)
                     )
                 }
                 Surface(
@@ -131,8 +157,107 @@ fun CounterPage(viewModel: MainViewModel, habitId: Int) {
                     Icon(
                         modifier = Modifier.size(160.dp),
                         imageVector = Icons.Filled.Add,
-                        contentDescription = stringResource(id = R.string.add_habit_description)
+                        contentDescription = stringResource(id = R.string.count_plus_one)
                     )
+                }
+            }
+        }
+        FloatingActionButton(
+            onClick = {
+                showPopupWindow = !showPopupWindow
+            },
+            Modifier
+                .align(Alignment.BottomEnd)
+                .padding(12.dp)
+                .clip(RoundedCornerShape(45.dp))
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Edit,
+                contentDescription = stringResource(id = R.string.edit_habit_description),
+                modifier = Modifier.size(40.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun EditHabitDialog(viewModel: MainViewModel, habit: Habit, onDismiss: () -> Unit,) {
+    val scope = rememberCoroutineScope()
+    val habitName = habit.name
+    var editHabitName by remember {
+        mutableStateOf(habitName)
+    }
+    var editHabitGoal by remember {
+        if (habit.goal == null){
+            mutableStateOf("0")
+        }
+        else {
+            mutableStateOf(habit.goal.toString())
+        }
+    }
+    val context = LocalContext.current
+    Dialog(
+        onDismissRequest = onDismiss
+    ) {
+        Surface {
+            Column {
+                OutlinedTextField(
+                    value = editHabitName,
+                    label = { Text("Habit Name") },
+                    onValueChange = { editHabitName = it.take(14) },
+                    textStyle = TextStyle(fontWeight = FontWeight.Bold, fontSize = 30.sp),
+                    maxLines = 1,
+                    modifier = Modifier.padding(8.dp),
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                )
+                OutlinedTextField(
+                    value = editHabitGoal,
+                    label = { Text("Daily Goal") },
+                    onValueChange = { editHabitGoal = it.take(4) },
+                    textStyle = TextStyle(fontWeight = FontWeight.Bold, fontSize = 30.sp),
+                    maxLines = 1,
+                    modifier = Modifier.padding(8.dp),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                )
+                Row (
+                    Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ){
+                    IconButton(
+                        onClick = { onDismiss() },
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Cancel,
+                            contentDescription = stringResource(id = R.string.cancel),
+                            modifier = Modifier
+                                .size(40.dp)
+                        )
+                    }
+                    IconButton(
+                        onClick = {
+                            val habitGoalInt = if (editHabitGoal == ""){
+                                null
+                            } else {
+                                editHabitGoal.toInt()
+                            }
+                            if (!viewModel.updateHabitClicked(habit, editHabitName, habitGoalInt)) {
+                                scope.launch {
+                                    Utils.showToastShort(context, R.string.habit_already_exists)
+                                }
+                            } else {
+                                onDismiss()
+                            }
+                        },
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.CheckCircle,
+                            contentDescription = stringResource(id = R.string.confirm),
+                            modifier = Modifier
+                                .size(40.dp)
+                        )
+                    }
                 }
             }
         }
--- app/src/main/java/com/pleavinseven/ui/HabitsPage.kt
+++ app/src/main/java/com/pleavinseven/ui/HabitsPage.kt
@@ -1,11 +1,11 @@
 package com.pleavinseven.ui
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,13 +17,13 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material3.Button
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -36,17 +36,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Popup
-import androidx.compose.ui.window.PopupProperties
+import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavController
 import com.pleavinseven.R
 import com.pleavinseven.utils.Utils
@@ -75,8 +74,9 @@ fun HabitsPage(viewModel: MainViewModel, navController: NavController) {
                 .clip(RoundedCornerShape(45.dp))
         ) {
             Icon(
-                imageVector = Icons.Filled.AddCircle,
-                contentDescription = stringResource(id = R.string.confirm)
+                imageVector = Icons.Rounded.Add,
+                contentDescription = stringResource(id = R.string.confirm),
+                modifier = Modifier.size(40.dp)
             )
         }
     }
@@ -145,56 +145,67 @@ fun AddHabitPopUp(viewModel: MainViewModel, onDismiss: () -> Unit) {
         mutableStateOf("")
     }
     val context = LocalContext.current
-    Popup(alignment = Alignment.Center, properties = PopupProperties(
-        focusable = true,
-        dismissOnClickOutside = true,
-        dismissOnBackPress = true,
-    ), onDismissRequest = { onDismiss() }) {
-        OutlinedCard(
-            elevation = CardDefaults.cardElevation(6.dp),
-            modifier = Modifier
-                .size(250.dp)
-                .background(Color.Transparent),
-            shape = RoundedCornerShape(35.dp),
-        ) {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Transparent)
-            ) {
-                Column {
-                    Text(
-                        text = stringResource(id = R.string.add_habit_name),
-                        textAlign = TextAlign.Center,
-                        style = MaterialTheme.typography.headlineLarge,
+    Dialog(
+        onDismissRequest = onDismiss
+    ) {
+        Surface {
+            Column {
+                OutlinedTextField(
+                    value = habitName,
+                    label = { Text("Habit Name") },
+                    onValueChange = { habitName = it.take(14) },
+                    textStyle = TextStyle(fontWeight = FontWeight.Bold, fontSize = 30.sp),
+                    maxLines = 1,
+                    modifier = Modifier.padding(8.dp),
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                )
+                OutlinedTextField(
+                    value = habitGoal,
+                    label = { Text("Daily Goal") },
+                    onValueChange = { habitGoal = it.take(4) },
+                    textStyle = TextStyle(fontWeight = FontWeight.Bold, fontSize = 30.sp),
+                    maxLines = 1,
+                    modifier = Modifier.padding(8.dp),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                )
+                Row (
+                    Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ){
+                    IconButton(
+                        onClick = { onDismiss() },
                         modifier = Modifier.padding(8.dp)
-                    )
-                    OutlinedTextField(
-                        value = habitName,
-                        onValueChange = { habitName = it.take(14) },
-                        textStyle = TextStyle(fontWeight = FontWeight.Bold, fontSize = 30.sp),
-                        maxLines = 1,
-                        modifier = Modifier.padding(8.dp),
-                        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-                    )
-                    Box(
-                        contentAlignment = Alignment.BottomEnd,
-                        modifier = Modifier
-                            .padding(8.dp)
-                            .fillMaxWidth(1f)
                     ) {
-                        Button(onClick = {
-                            if (!viewModel.createHabitClicked(habitName)) {
+                        Icon(
+                            imageVector = Icons.Filled.Cancel,
+                            contentDescription = stringResource(id = R.string.cancel),
+                            modifier = Modifier
+                                .size(40.dp)
+                        )
+                    }
+                    IconButton(
+                        onClick = {
+                            val habitGoalInt = if (habitGoal == ""){
+                                null
+                            } else {
+                                habitGoal.toInt()
+                            }
+                            if (!viewModel.createHabitClicked(habitName, habitGoalInt)) {
                                 scope.launch {
                                     Utils.showToastShort(context, R.string.habit_already_exists)
                                 }
                             } else {
                                 onDismiss()
                             }
-                        }) {
-                            Text(text = stringResource(id = R.string.create_habit))
-                        }
+                        },
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.CheckCircle,
+                            contentDescription = stringResource(id = R.string.confirm),
+                            modifier = Modifier
+                                .size(40.dp)
+                        )
                     }
                 }
             }
