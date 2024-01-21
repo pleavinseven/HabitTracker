828d3b6230a68f73dd2e6f56c7c3fda61afc6007
pleavin <pleavinseven@gmail.com>
2024-01-21 16:55:11 +0100

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,26 +52,26 @@ android {
 
 dependencies {
 
-    val composeVersion = "2.6.2"
-    val workVersion = "2.8.1"
+    val composeVersion = "2.7.0"
+    val workVersion = "2.9.0"
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$composeVersion")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$composeVersion")
-    implementation("androidx.activity:activity-ktx:1.8.0")
+    implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.test:monitor:1.6.1")
-    implementation("androidx.navigation:navigation-compose:2.7.4")
+    implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation("androidx.compose.material3:material3:1.1.2")
-    implementation("androidx.compose.material:material-icons-extended:1.5.3")
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
     implementation("androidx.work:work-runtime-ktx:$workVersion")
 
-    val roomVersion = "2.5.2"
+    val roomVersion = "2.6.1"
 
     implementation("androidx.room:room-runtime:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
