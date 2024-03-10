2875a1b79e7a5592d599829e0336d3aa473c63f2
pleavin <pleavinseven@gmail.com>
2024-03-10 23:07:18 +0100

    basic calendar added to log page with viewmodel.

--- app/src/main/java/com/pleavinseven/ui/HabitTrackerApp.kt
+++ app/src/main/java/com/pleavinseven/ui/HabitTrackerApp.kt
@@ -5,11 +5,12 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.pleavinseven.viewmodels.HabitViewModel
+import com.pleavinseven.viewmodels.LazyCalendarViewModel
 import com.pleavinseven.viewmodels.TimeLogViewModel
 
 @Composable
 fun HabitTrackerApp(
-    habitViewModel: HabitViewModel, timeLogViewModel: TimeLogViewModel
+    habitViewModel: HabitViewModel, timeLogViewModel: TimeLogViewModel, lazyCalendarViewModel: LazyCalendarViewModel
 ) {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = "HabitsPage") {
@@ -20,7 +21,7 @@ fun HabitTrackerApp(
             CounterPage(habitViewModel, timeLogViewModel, navController)
         }
         composable("LogPage") {
-            LogPage(navController)
+            LogPage(navController, timeLogViewModel, lazyCalendarViewModel, habitViewModel)
         }
         composable("Settings") {
             Settings(navController)
