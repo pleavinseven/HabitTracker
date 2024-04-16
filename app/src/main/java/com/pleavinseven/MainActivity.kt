8663d08690b5ae8a48c71be225c9394a7fed8175
pleavin <pleavinseven@gmail.com>
2024-04-17 00:00:26 +0200

    add preview, tidy up code

--- app/src/main/java/com/pleavinseven/MainActivity.kt
+++ app/src/main/java/com/pleavinseven/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : ComponentActivity() {
             TimeLogViewModelFactory(repository)
         }
         val logPageViewModel: LogPageViewModel by viewModels {
-            LogPageViewModelFactory(repository)
+            LogPageViewModelFactory(repository, habitViewModel)
         }
         setContent {
             HabitTrackerTheme {
