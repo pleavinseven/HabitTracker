77429649e3f9334161cce7aa17cee36fe8045e69
pleavin <pleavinseven@gmail.com>
2024-04-16 23:09:30 +0200

    basic graph added

--- app/src/main/java/com/pleavinseven/ui/LogPage.kt
+++ app/src/main/java/com/pleavinseven/ui/LogPage.kt
@@ -1,5 +1,6 @@
 package com.pleavinseven.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,22 +13,33 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.patrykandpatrick.vico.compose.axis.horizontal.rememberBottomAxis
+import com.patrykandpatrick.vico.compose.axis.vertical.rememberStartAxis
+import com.patrykandpatrick.vico.compose.chart.CartesianChartHost
+import com.patrykandpatrick.vico.compose.chart.layer.rememberColumnCartesianLayer
+import com.patrykandpatrick.vico.compose.chart.rememberCartesianChart
+import com.patrykandpatrick.vico.core.model.CartesianChartModelProducer
+import com.patrykandpatrick.vico.core.model.ExtraStore
+import com.patrykandpatrick.vico.core.model.columnSeries
 import com.pleavinseven.composables.MyTheme
+import com.pleavinseven.model.entities.DailyCount
 import com.pleavinseven.viewmodels.LogPageViewModel
 
 @Composable
@@ -36,8 +48,10 @@ fun LogPage(
     viewModel: LogPageViewModel,
 ) {
     viewModel.getDataForGraphs()
+    viewModel.timeLogList
     MyTheme(navController = navController) {
         LazyCalendar(viewModel)
+        DailyCountBarChart(viewModel.countList, viewModel)
         LazyVerticalGrid(
             columns = GridCells.Fixed(2)
         ) {
@@ -95,7 +109,6 @@ fun LazyCalendar(viewModel: LogPageViewModel) {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CalendarCard(
     day: String,
@@ -120,5 +133,44 @@ fun CalendarCard(
                 style = MaterialTheme.typography.bodySmall,
             )
         }
+    }
+}
+
+@Composable
+fun DailyCountBarChart(countList: List<DailyCount>, viewModel: LogPageViewModel) {
+    Card(
+        modifier = Modifier.padding(12.dp),
+        border = BorderStroke(2.dp, Color.White)
+    ) {
+        var counts by remember { mutableStateOf(countList.map { it.count }) }
+        var dates by remember { mutableStateOf(countList.map { it.day }) }
+        var monthLength by remember(viewModel) { mutableStateOf(viewModel.getMonthLength()) }
+        val modelProducer = remember { CartesianChartModelProducer.build() }
+        monthLength = viewModel.getMonthLength()
+        val labelListKey = ExtraStore.Key<Array<String>>()
+        LaunchedEffect(Unit) {
+            modelProducer.tryRunTransaction {
+                counts = countList.map { it.count }
+                dates = countList.map { it.day }
+                columnSeries {
+//                    for each series x will be the dates that it occurred and y the amount
+//                    if x is not given then it starts from 0
+//                    series(y = listOf(6, 1, 9, 3))
+//                    series(x = listOf(1, 2, 3, 4), y = listOf(2, 5, 3, 4))
+                    series(x = dates, y = counts)
+                }
+                updateExtras {
+                    it[labelListKey] = monthLength
+                }
+            }
+        }
+        CartesianChartHost(
+            rememberCartesianChart(
+                rememberColumnCartesianLayer(),
+                startAxis = rememberStartAxis(),
+                bottomAxis = rememberBottomAxis(),
+            ),
+            modelProducer,
+        )
     }
 }
